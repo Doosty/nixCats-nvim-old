364af341cdf546b88be0e48b180d274c5726e08d
Doosty <github-action@actions-template-sync.noreply.github.com>
2023-12-03 12:21:49 +0000

    chore(template): merge template changes :up:

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,40 +1,39 @@
+# The following is the overlays/default.nix file.
+# you may copy paste it into a file then include it in your flake.nix
+# to add new overlays you should follow 
+# the directions inside the comment blocks.
+# it is done this way for convenience but you could do it another way.
+
+# When importing these overlays from one nixCats into another using its otherOverlays,
+# you should use the utils.mergeOverlays function to combine them.
+# This will allow you not to worry about other nixCats overlay
+# imports from having import naming conflicts with your own.
+
+/*
+This file imports overlays defined in the following format.
+Plugins will still only be downloaded if included in a category.
+You may copy paste this example into a new file and then import that file here.
+*/
+# Example overlay:
+/*
+importName: inputs: let
+  overlay = self: super: { 
+    ${importName} = {
+      # define your overlay derivations here
+    };
+  };
+in
+overlay
+*/
 inputs: let 
   overlaySet = {
 
     # this is how you would add another overlay file
     # for if your customBuildsOverlay gets too long
-    customBuilds = import ./customBuildsOverlay.nix inputs;
+    # the name here will be the name used when importing items from it in your flake.
+    # i.e. these items will be accessed as pkgs.nixCatsBuilds.thenameofthepackage
+    nixCatsBuilds = import ./customBuildsOverlay.nix;
 
-    # This overlay grabs all the inputs named in the format
-    # `plugins-<pluginName>`
-    # Once we add this overlay to our nixpkgs, we are able to
-    # use `pkgs.neovimPlugins`, which is a set of our plugins.
-    # Source: https://github.com/Quoteme/neovim-flake/blob/34c47498114f43c243047bce680a9df66abfab18/flake.nix#L42C8-L42C8
-    standardPluginOverlay = self: super:
-    let
-      inherit (super.vimUtils) buildVimPlugin;
-      plugins = builtins.filter
-        (s: (builtins.match "plugins-.*" s) != null)
-        (builtins.attrNames inputs);
-      plugName = input:
-        builtins.substring
-          (builtins.stringLength "plugins-")
-          (builtins.stringLength input)
-          input;
-      buildPlug = name: buildVimPlugin {
-        pname = plugName name;
-        version = "master";
-        src = builtins.getAttr name inputs;
-      };
-    in
-    {
-      neovimPlugins = builtins.listToAttrs (map
-        (plugin: {
-          name = plugName plugin;
-          value = buildPlug plugin;
-        })
-        plugins);
-    };
   };
 in
-builtins.attrValues overlaySet
+builtins.attrValues (builtins.mapAttrs (name: value: (value name inputs)) overlaySet)
